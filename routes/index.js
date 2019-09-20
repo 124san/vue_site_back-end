4dd5d3fce5fbaaffe1230a969befd1a1e8456c53
Dehou Kong <dehou.kong@mail.utoronto.ca>
2019-09-20 14:54:30 -0400

    add password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// For passwd hashing
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -22,34 +26,6 @@ router.get('/userlist', function(req, res) {
 });
 
 /* POST to Add User Service */
-router.get('/addOz', function(req, res) {
-
-  // Set our internal DB variable
-  var connection = req.db;
-
-  // Get our form values. These rely on the "name" attributes
-  var userName = 'ozsama';
-  var userEmail = 'ozsama@ayaya.com';
-
-  // We're not populating ID here because it should be auto-incrementing
-  var newUser = { username: userName, email: userEmail }
-
-  // Submit to the DB
-  connection.query('INSERT INTO usercollection SET ?', newUser, function (error, results, fields) {
-      if (error) {
-          // If it failed, return error
-          res.send("There was a problem adding the information to the database.");
-      }
-      else {
-          // If it worked, set the header so the address bar doesn't still say /adduser
-          //res.location("userlist");
-          // And forward to success page
-          res.redirect("userlist");
-      }
-  });
-});
-
-/* POST to Add User Service */
 router.post('/adduser', function(req, res) {
 
   // Set our internal DB variable
@@ -58,9 +34,17 @@ router.post('/adduser', function(req, res) {
   // Get our form values. These rely on the "name" attributes
   var userName = req.body.username;
   var userEmail = req.body.useremail;
+  var password = req.body.password;
+  let hashedPW = '';
+  bcrypt.hash(password, saltRounds, (err, hashed) => {
+    if (err) {
+      throw err;
+    }
+    hashedPW = hashed;
+  })
 
   // We're not populating ID here because it should be auto-incrementing
-  var newUser = { username: userName, email: userEmail }
+  var newUser = { username: userName, email: userEmail, password: hashedPW}
 
   // Submit to the DB
   connection.query('INSERT INTO usercollection SET ?', newUser, function (error, results, fields) {
