038de027b6ce782840e108ea19c65ffb89a8275d
Dehou Kong <dehou.kong@mail.utoronto.ca>
2019-09-20 10:58:24 -0400

    add initial

---
+++ config.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const configPath = './config.json';
+const parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+
+// We have to export each object in order to access them separately
+exports.sqluser = parsed.mysql_username;
+exports.sqlpw = parsed.mysql_pw;
---
+++ routes/index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Im gonna say the n word!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  connection.query('SELECT * FROM usercollection', function(err, rows, fields) {
+    res.render('userlist', {
+        "userlist" : rows,
+    });
+  });
+});
+
+/* POST to Add User Service */
+router.get('/addOz', function(req, res) {
+
+  // Set our internal DB variable
+  var connection = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  var userName = 'ozsama';
+  var userEmail = 'ozsama@ayaya.com';
+
+  // We're not populating ID here because it should be auto-incrementing
+  var newUser = { username: userName, email: userEmail }
+
+  // Submit to the DB
+  connection.query('INSERT INTO usercollection SET ?', newUser, function (error, results, fields) {
+      if (error) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // If it worked, set the header so the address bar doesn't still say /adduser
+          //res.location("userlist");
+          // And forward to success page
+          res.redirect("userlist");
+      }
+  });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+  // Set our internal DB variable
+  var connection = req.db;
+
+  // Get our form values. These rely on the "name" attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // We're not populating ID here because it should be auto-incrementing
+  var newUser = { username: userName, email: userEmail }
+
+  // Submit to the DB
+  connection.query('INSERT INTO usercollection SET ?', newUser, function (error, results, fields) {
+      if (error) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          // If it worked, set the header so the address bar doesn't still say /adduser
+          //res.location("userlist");
+          // And forward to success page
+          res.redirect("userlist");
+      }
+  });
+});
+
+module.exports = router;
