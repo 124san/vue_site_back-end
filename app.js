4a190f317bc9e3f955c4a88bb2a8ae775dd9ee36
Dehou Kong <dehou.kong@mail.utoronto.ca>
2019-12-01 00:27:46 -0500

    fix cookie

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('trust proxy', 1)
 // ---------- Cors --------
 app.options('*', cors({origin: 'https://sz124san.herokuapp.com'}))
 app.use(cors({origin: 'https://sz124san.herokuapp.com'}))
