c6d3ccb729c790ae2cea9e67ceccfe2f8dd59839
Dehou Kong <124kdaniel@gmail.com>
2020-01-22 01:08:03 -0500

    bbbbb

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieSession({
   name: 'mysession',
   keys: [process.env.SESSION_KEY || 'akey'],
   httpOnly: true,
-  secure: false,
+  secure: true,
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 // ---------------------- Passport ---------------------
